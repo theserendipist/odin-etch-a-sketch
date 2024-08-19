ca6ec008eea64a2530ed39bc549a6cac4863640d
Hedonysos <167946166+theserendipist@users.noreply.github.com>
2024-08-19 16:56:30 +0200

    Add New Grid feature w/ choice of grid size

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
 // Etch-A-Sketch Game
 
-//Basic setup of 16*16 grid
+// Create grid function
+function newGrid(n) {
+    for (let i = 0; i < n; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (let j = 0; j < n; j++) {
+            const div = document.createElement('div');
+            div.classList.add('square');
+        
+            row.appendChild(div);
+       
+            // Add hover effect
+            div.addEventListener("mouseenter", () => {
+                div.style.backgroundColor = "red";
+            });
+        }
+    };
+}
+
+// Intial 16*16 grid setup
 const container = document.querySelector('#container');
 
-const nSquares = 16*16
+newGrid(n = 16);
 
-// Create grid
-for (let i = 0; i < nSquares; i ++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
+// New Sketch function
+const btn = document.querySelector('#new');
 
-    container.appendChild(div);
+btn.addEventListener("click", () => {
+    // Empty container
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+
+    // Ask for new grid size and check if it's between 0 and 100
+    let nSquares = prompt('How many squares should there be on one side of the grid?')
+    while (nSquares < 0 || nSquares > 100) {
+        nSquares = prompt('Please type a number between 0 and 100!')
+    }
     
-    // Add hover effect
-    div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "red";
-    });
-}
+    // Draw new grid
+    newGrid(n = nSquares);
+})
