dae3d056a08e5b40df6185e4865e9ade9da428de
Hedonysos <167946166+theserendipist@users.noreply.github.com>
2024-08-19 13:46:28 +0200

    Setup basic 16*16 grid

---
+++ script.js
@@ -0,0 +1,12 @@
+// Etch-A-Sketch Game
+
+//Basic setup of 16*16 grid
+const container = document.querySelector('#container');
+
+const nSquares = 16*16
+
+for (let i = 0; i < nSquares; i ++) {
+    const div = document.createElement('div');
+    div.classList.add('square');
+    container.appendChild(div);
+}
